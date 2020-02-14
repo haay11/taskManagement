b0b5a8572bb010102117bb0eca14355a7ce6c177
haay11 <haay03@gmail.com>
2020-02-14 17:06:36 +0900

    first commit

--- app/src/main/java/com/example/taskmanagement/storage/TaskStorage.java
+++ app/src/main/java/com/example/taskmanagement/storage/TaskStorage.java
@@ -9,32 +9,32 @@ import java.util.List;
 
 public interface TaskStorage {
 
-    public void getUserList(OnDataReceivedListener listener);
-    public void getNewTaskUserInfo(String userEmail, @Nullable String userName, OnDataTaskUserInfoReceivedListener listener);
-    public void getUserName(OnDataNameReceivedListener listener);
-    public void getOrderBoxPubl(@Nullable String uid,@Nullable String timeStamp, OnDataPublisherReceivedListener listener);
-    public void insertBusinessOrder(TaskRequest taskRequest);
-    public void insertUserInfo(TaskUserInfo taskUserInfo);
-    public void updateApprovalStatus(String uid, String timeStamp, int approvalStatus, String sender);
-    public void passPushTokenToServer();
-    public void getUserInfo(String uid, OnDataUserInfoReceivedListener listener);
-    public void updateStatus(String uid, String timeStamp, int status);
-    public void insertReason(String senderUid, String timeStamp, String reason);
+    void getUserList(OnDataReceivedListener listener);
+    void getNewTaskUserInfo(String userEmail, @Nullable String userName, OnDataTaskUserInfoReceivedListener listener);
+    void getUserName(OnDataNameReceivedListener listener);
+    void getOrderBoxPubl(@Nullable String uid,@Nullable String timeStamp, OnDataPublisherReceivedListener listener);
+    void insertBusinessOrder(TaskRequest taskRequest);
+    void insertUserInfo(TaskUserInfo taskUserInfo);
+    void updateApprovalStatus(String uid, String timeStamp, int approvalStatus, String sender);
+    void passPushTokenToServer();
+    void getUserInfo(String uid, OnDataUserInfoReceivedListener listener);
+    void updateStatus(String uid, String timeStamp, int status);
+    void insertReason(String senderUid, String timeStamp, String reason);
 
 
-    public interface OnDataTaskUserInfoReceivedListener{
+    interface OnDataTaskUserInfoReceivedListener{
         void OnDataReceived(String[] newTaskUserInfos);
     }
-    public interface OnDataReceivedListener{
+    interface OnDataReceivedListener{
         void OnDataReceived(List<String> userEmailList);
     }
-    public interface OnDataNameReceivedListener{
+    interface OnDataNameReceivedListener{
         void OnDataReceived(List<String> userNameList);
     }
-    public interface OnDataPublisherReceivedListener{
+    interface OnDataPublisherReceivedListener{
         void OnDataReceived(List<TaskRequest> taskRequest);
     }
-    public interface OnDataUserInfoReceivedListener {
+    interface OnDataUserInfoReceivedListener {
         void OnDataReceived(List<TaskUserInfo> userNameList);
     }
 }
